cc157757bbdd586a53540aedffb2f0d73ff91df1
Paul-Beniamin ILIOAICA (88064) <paul.ilioaica@stud.etti.upb.ro>
2021-04-15 23:19:04 +0300

    Add dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,9 +1,7 @@
 import os
+
 import numpy as np
-import torch
-import pandas as pd
 from torch.utils.data import Dataset
-import random
 
 LABELS = ['Valence', 'Arousal', 'Dominance']
 
@@ -13,15 +11,16 @@ class DEAP(Dataset):
         self.label_dir = label_path
         self.root_dir = root_dir
         assert os.path.exists(os.path.join(self.root_dir)), "Path to files cannot be found"
-        assert os.path.exists(os.path.join(self.label_dir)), "Path to files cannot be found"
+        assert os.path.exists(os.path.join(self.label_dir)), "Path to labels cannot be found"
         self.paths = sorted([os.path.join(self.root_dir, path) for path in os.listdir(self.root_dir) if 'data' in path])
-        self.labels = sorted([os.path.join(self.label_dir, path) for path in os.listdir(self.root_dir) if 'label' in path])
+        self.labels = sorted(
+            [os.path.join(self.label_dir, path) for path in os.listdir(self.root_dir) if 'label' in path])
 
     def __len__(self):
         return len(self.paths)
 
     def __getitem__(self, item):
-        eeg_data = np.load(self.path[item])
+        eeg_data = np.load(self.paths[item])
 
         corr_matrix = np.corrcoef(eeg_data[:32, :])
         corr_matrix = np.abs(corr_matrix)
